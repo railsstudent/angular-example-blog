b43afb69bdfb9b06b46e704b4cc875cd0a521cb9
Connie Leung <cleung334@gmail.com>
2025-09-10 07:46:08 +0800

    chore: enable suffix to schematic

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class AppComponent {
+  protected readonly title = signal('angular-example-blog');
+}
